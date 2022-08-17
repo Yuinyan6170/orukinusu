618e3e6edea26be3b894aded166fd8e3a4e7e4e5
Yuinyan6170 <108215265+Yuinyan6170@users.noreply.github.com>
2022-08-17 12:31:50 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const discord = require('discord.js');
+
+const token = '';
+
+const client = new discord.Client({intents: [
+    discord.GatewayIntentBits.DirectMessageReactions,
+    discord.GatewayIntentBits.DirectMessageTyping,
+    discord.GatewayIntentBits.DirectMessages,
+    discord.GatewayIntentBits.GuildBans,
+    discord.GatewayIntentBits.GuildEmojisAndStickers,
+    discord.GatewayIntentBits.GuildIntegrations,
+    discord.GatewayIntentBits.GuildInvites,
+    discord.GatewayIntentBits.GuildMembers,
+    discord.GatewayIntentBits.GuildMessageReactions,
+    discord.GatewayIntentBits.GuildMessageTyping,
+    discord.GatewayIntentBits.GuildMessages,
+    discord.GatewayIntentBits.GuildPresences,
+    discord.GatewayIntentBits.GuildScheduledEvents,
+    discord.GatewayIntentBits.GuildVoiceStates,
+    discord.GatewayIntentBits.GuildWebhooks,
+    discord.GatewayIntentBits.Guilds,
+    discord.GatewayIntentBits.MessageContent
+]});
+
+process.on('uncaughtException', (err) => {
+    client.channels.fetch('1008973466772439120')
+    .then(channel => {
+        channel.send('```\n' + err.stack + '\n```');
+    });
+});
+
+client.on('ready', async c => {
+    client.guilds.cache.forEach(async (key, value) => {
+        await client.application.commands.set([{name: 'test', description: 'this is a test for slash command'}], value.id);
+    });
+});
+
+client.on('messageCreate', async m => {
+    if (m.author.bot) return;
+});
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) {
+        if (interaction.commandName === 'test') {
+            await interaction.reply({content: 'できたァァァァァァ', ephemeral: false});
+        }
+    }
+});
+
+client.login(token);
